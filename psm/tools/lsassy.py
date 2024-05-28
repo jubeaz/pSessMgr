3b5c0b235b1a5481b4bf7338b573f29cf39dc258
jubeaz <pascal.auillans@gmail.com>
2024-05-28 02:11:17 +0200

    update

---
+++ psm/tools/lsassy.py
@@ -0,0 +1,8 @@
+import os
+from psm.logger import psm_logger
+from psm.tools.super.toolsuper import PSMToolSuper
+
+
+class PSMTool(PSMToolSuper):
+    def __init__(self):
+        super().__init__('lsassy', ["~/.config/lsassy"])
